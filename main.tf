a5679c26c4e763136d19ee992fb60ef131e4e15a
demodev-at-791932721466 <kaderhouchi@gmail.com>
2021-10-03 13:18:03 -0400

    add tfm files to repo

---
+++ main.tf
@@ -0,0 +1,54 @@
+terraform {
+  required_version = ">= 0.11.0"
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+# security group
+resource "aws_security_group" "sginout" {
+  name = "sgsshpermit"
+
+  #Incoming traffic
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["65.94.232.139/32"] # replace with your ip address
+  }
+
+  #Outgoing traffic
+  egress {
+    from_port = 0
+    protocol = "-1"
+    to_port = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Key pair
+resource "aws_key_pair" "tfm_key" {
+  key_name = "ssh_key"
+  public_key = file("/Users/kader/.ssh/mylab_tfm.pub")
+}
+
+#Create aws instance
+resource "aws_instance" "mylinux2021" {
+  ami               = var.ami_id
+  instance_type     = var.instance_type
+  availability_zone = "${var.aws_region}a"
+  key_name = aws_key_pair.tfm_key.key_name
+  security_groups = ["sgsshpermit"]
+
+  tags = {
+    Name = "${var.name}"
+  }
+}
---
+++ outputs.tf
@@ -0,0 +1,3 @@
+output "public_dns" {
+  value = aws_instance.mylinux2021.public_dns
+}
